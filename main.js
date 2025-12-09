9fcc2b207f2287d298e9598703df13f4b74b2a48
Rahul Tiwari <128848653+rrtiwari@users.noreply.github.com>
2025-12-09 08:39:53 +0530

    Implement server response and logging

--- main.js
+++ main.js
@@ -2,13 +2,14 @@ const http = require('http');
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  // TODO: Send the ""Hello, world!"" response
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, world!\n');
 });
 
 server.listen(port, () => {
-  // TODO: Log a message to the console when the server starts listening on the port
+  console.log(`Server is listening on port ${port}`);
 });
 
-// DO NOT EDIT BELOW THIS LINE
-
 module.exports = { server }
+
